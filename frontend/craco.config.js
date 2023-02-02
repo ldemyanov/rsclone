23f1a6a4ff72b1136d61491dbdd6c61d4cbcdb72
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2023-02-02 11:58:05 +0300

    feat: add rules for assets, fix: rename branch

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -4,6 +4,7 @@ module.exports = {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
       '@src': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
   },
 };
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react';
+import logo from '@assets/images/logo.png';
+import styles from './styles.module.css';
 
 export const Header: FC = () => {
   return <header>Header</header>;

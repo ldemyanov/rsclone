4424a6bdc85d9ef1557fcb0557564065546356f6
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2023-02-02 11:25:43 +0300

    feat: add alias

--- frontend/src/layouts/mainLayout/index.tsx
+++ frontend/src/layouts/mainLayout/index.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from 'react';
-import { Header } from '../../components/Header';
-import { Footer } from '../../components/Footer';
+import { Footer } from '@components/Footer';
+import { Header } from '@components/Header';
 
 interface MainLayoutProps {
   children: ReactNode;

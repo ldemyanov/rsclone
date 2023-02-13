f540373771e3807593c0e36211cbab43be517a7d
RussianBoy64 <kozlachkovvd@gmail.com>
2023-02-13 18:00:50 +0400

    feat: cursor styles for draw and correct color detection with pippette tool

--- frontend/src/components/Login/Authorization/SectionAuthorization/index.tsx
+++ frontend/src/components/Login/Authorization/SectionAuthorization/index.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Input, InputPlaceholders } from '@components/UI/Input';
 import { setName } from '@src/redux/reducers/authorizationReducer';
+import { RootState } from '@src/redux/store';
 import PlayerIconLeo from '@assets/images/player_icon_leo.png';
 import reload from '@assets/images/reload.svg';
 import styles from './styles.module.css';
 
 export const SectionAuthorization: FC = () => {
   const dispatch = useDispatch();
+  const { name } = useSelector((state: RootState) => state.auth);
 
   const changeName = (e: React.FormEvent<HTMLInputElement>) => {
     dispatch(setName(e.currentTarget.value));
@@ -21,7 +23,7 @@ export const SectionAuthorization: FC = () => {
           <img src={reload} alt="reload" />
         </button>
       </div>
-      <Input placeholder={InputPlaceholders.name} onChange={() => null} value="" />
+      <Input placeholder={InputPlaceholders.name} onChange={changeName} value={name} />
     </section>
   );
 };

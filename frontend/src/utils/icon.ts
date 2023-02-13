abb4b080e901d9bd3979bc688e8c086408f0eb8f
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2023-02-13 17:59:18 +0300

    refactore: code

--- frontend/src/utils/icon.ts
+++ frontend/src/utils/icon.ts
@@ -3,14 +3,12 @@ import Cat from '@assets/images/icon/bird.png';
 import Dog from '@assets/images/icon/dog.png';
 import Fox from '@assets/images/icon/bird.png';
 import KillerWhale from '@assets/images/icon/killer_whale.png';
-import Koala from '@assets/images/icon/koala.png';
 import Lion from '@assets/images/icon/lion.png';
 import Monkey from '@assets/images/icon/monkey.png';
 import Mouse from '@assets/images/icon/mouse.png';
 import Panda from '@assets/images/icon/panda.png';
-import Snake from '@assets/images/icon/snake.png';
 import Tiger from '@assets/images/icon/tiger.png';
 import Whale from '@assets/images/icon/whale.png';
 import Wolf from '@assets/images/icon/wolf.png';
 
-export const icons = [Bird, Cat, Dog, Fox, KillerWhale, Koala, Lion, Monkey, Mouse, Panda, Snake, Tiger, Whale, Wolf];
+export const icons = [Bird, Cat, Dog, Fox, KillerWhale, Lion, Monkey, Mouse, Panda, Tiger, Whale, Wolf];

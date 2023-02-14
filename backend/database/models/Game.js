f68c9b355c671321f322c5bf87da44a59b896b9c
ldemyanov <leonidemyanov@gmail.com>
2023-02-14 22:44:33 +0800

    fix: remove propeties required

--- backend/database/models/Game.js
+++ backend/database/models/Game.js
@@ -1,17 +1,17 @@
 module.exports = mongoose => {
   const gameSchema = mongoose.Schema(
     {
-      roomId: {type: Boolean, required: true},
+      roomId: {type: String, required: true},
       isGameStarted: {type: Boolean, required: true},
       gameStage: {type: String, required: true},
       words: [
         {
-          word: {type: String, required: true, default: ''},
-          writerId: {type: String, required: false},
-          img: {type: String, required: false},
-          painterId: {type: String, required: false},
-          response: {type: String, required: false},
-          responserId: {type: String, required: false},
+          word: String,
+          writerId: String,
+          img: String,
+          painterId: String,
+          response: String,
+          responserId: String,
         }
       ]
     }
--- backend/database/models/GameWord.js
+++ backend/database/models/GameWord.js
@@ -1,12 +1,12 @@
 module.exports = mongoose => {
   const gameWordSchema = mongoose.Schema(
     {
-      word: {type: String, required: true, default: ''},
-      writerId: {type: String, required: false},
-      img: {type: String, required: false},
-      painterId: {type: String, required: false},
-      response: {type: String, required: false},
-      responserId: {type: String, required: false},
+      word: String,
+      writerId: String,
+      img: String,
+      painterId: String,
+      response: String,
+      responserId: String,
     }
   );
   

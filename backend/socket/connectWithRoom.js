a1df7e265b6be0c92f806f73be2afafa242e3824
ldemyanov <leonidemyanov@gmail.com>
2023-02-14 22:47:23 +0800

    feat: start game, send words

--- backend/socket/connectWithRoom.js
+++ backend/socket/connectWithRoom.js
@@ -1,15 +1,16 @@
-const db = require("../database");
+const db = require('../database');
 
 async function connectWithRoom(socket) {
   const { roomId, userId } = socket.handshake.query;
+  const gameId = `${roomId}-game`;
   
-  socket.join(roomId)
+  socket.join(roomId);
   console.log(`${userId} joined room ${roomId}`);
 
   let room = await db.room.findOne({ roomId: roomId }).exec();
   let user = room.users.find((user) => user.userId === userId);
 
-  socket.to(roomId).emit("ROOM:JOIN", user);
+  socket.to(roomId).emit('ROOM:JOIN', user);
 
   socket.on('disconnecting', async (reason) => {
     // If the user closed the browser tab then "reason" equals "transport close" 
@@ -47,11 +48,17 @@ async function connectWithRoom(socket) {
 
     const res = {
       status: user.status, 
-      userId: user.userId
+      userId: user.userId,
     }
 
-    socket.emit("ROOM:STATUS", res);
-    socket.to(roomId).emit("ROOM:STATUS", res);
+    if (status === 'active') {
+      socket.join(gameId);
+    } else {
+      socket.leave(gameId);
+    }
+
+    socket.emit('ROOM:STATUS', res);
+    socket.to(roomId).emit('ROOM:STATUS', res);
   })
 
   socket.on('USER:SET_TYPE_GAME', async (typeGame) => {
@@ -62,6 +69,57 @@ async function connectWithRoom(socket) {
     socket.emit("ROOM:GET_TYPE_GAME", typeGame)
     socket.to(roomId).emit("ROOM:GET_TYPE_GAME", typeGame);
   })
+
+  socket.on('USER:START_GAME', async (req) => {
+    if (req.isGameStarted) {
+
+      room = await db.room.findOne({ roomId: roomId }).exec();
+      let words = [];
+
+      room.users.map(user => user.status === 'active' && words.push({
+        word: "",
+        writerId: user.userId,
+      }));
+
+      console.log(words);
+
+      const newGame = new db.game({
+        roomId: roomId,
+        isGameStarted: true,
+        gameStage: 'write',
+        words: words,
+      })
+      newGame.save();
+
+      socket.emit("ROOM:START_GAME", newGame);
+      socket.to(gameId).emit("ROOM:START_GAME", newGame);
+    }
+  })
+
+  socket.on('USER:SEND_WORD', async (word) => {
+
+    console.log(word);
+
+    const game = await db.game.findOne({ roomId: roomId }).exec();
+
+    for (let i = 0; i < game.words.length; i++) {
+      if (game.words[i].writerId === userId) {
+        game.words[i].word = word;
+        // TODO game.words[i].painterId = RANDOM ALGORITM
+        game.words[i].painterId = game.words.at(i-1).writerId;
+      }
+    }
+
+    await game.save();
+
+    let isFull = game.words.every((el) => el.word && el.painterId);
+    if (isFull) {
+      game.gameStage = 'draw';
+      await game.save();
+      socket.emit("ROOM:SEND_WORDS", game);
+      socket.to(gameId).emit("ROOM:SEND_WORDS", game);
+    }
+  });
 }
 
 module.exports = connectWithRoom;
--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -6,6 +6,31 @@ import { IPlayer } from '@src/types';
 
 let socket: null | Socket = null;
 
+// TODO remove or rename and move to types
+interface IWord {
+  word: string,
+  writerId: string
+}
+
+interface IWord2 extends IWord {
+  painterId: string,
+}
+
+// TODO remove or rename and move to types
+interface IGame {
+  roomId: string,
+  isGameStarted: boolean,
+  gameStage: string,
+}
+
+interface IGame1 extends IGame {
+  words: IWord[];
+}
+
+interface IGame2 extends IGame {
+  words: IWord2[];
+}
+
 export default function useSocket() {
 
   const dispatch = useAppDispatch();
@@ -28,6 +53,14 @@ export default function useSocket() {
       socket.on("ROOM:STATUS", (obj) => {
         console.log(obj)
       })
+
+      socket.on("ROOM:START_GAME", (gameObj: IGame1) => {
+        console.log("IGame1: ", gameObj);
+      })
+
+      socket.on("ROOM:SEND_WORDS", (gameObj: IGame2) => {
+        console.log("IGame2: ", gameObj);
+      })
     },
 
     excludeUser: (userId: string) => {
@@ -40,7 +73,21 @@ export default function useSocket() {
       if (socket) {
         socket.emit("USER:SET_STATUS", status);
       }
+    },
+
+    startGame: (req: { isGameStarted: boolean }) => {
+      if (socket) {
+        socket.emit("USER:START_GAME", req);
+      }
+    },
+
+    sendWord: (word: string) => {
+      if (socket) {
+        socket.emit("USER:SEND_WORD", word)
+      }
     }
   }
-
 }
+
+
+

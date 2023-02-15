c791894dc7c069e443f6118671c417786344d00e
RussianBoy64 <kozlachkovvd@gmail.com>
2023-02-15 22:22:27 +0400

    feat: gameInfo component

--- frontend/src/pages/GamePages/DrawPage/index.tsx
+++ frontend/src/pages/GamePages/DrawPage/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { ContentBorder } from '@components/UI/ContentBorder';
-import { GameProgress } from '@components/UI/GameProgress';
+import { GameProgress } from '@components/UI/GameInfo';
 import { Pallete } from '@components/Draw/Pallete';
 import { Tablet, TabletTitles } from '@components/Draw/Tablet';
 import { DrawTools } from '@components/Draw/DrawTools';
@@ -12,7 +12,7 @@ export const DrawPage: FC = () => {
   return (
     <ContentBorder>
       <section className={styles.container}>
-        <GameProgress currentStage={1} totalStages={3} />
+        <GameProgress currentStage={1} totalStages={3} readyPlayers={1} totalPlayers={3} />
         <Pallete />
         <div className={styles.column}>
           <Tablet title={TabletTitles.draw} />

25a185fabd3fe12802e2cda179d41a8a10dfe395
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2023-02-23 18:02:52 +0300

    feat: add name check

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -1,12 +1,21 @@
 const router = require("express").Router();
 const db = require("../database");
 
-router.get("/", async (req, res) => {
+router.get("/players", async (req, res) => {
+  const { roomId } = req.query;
+  console.log(roomId, "123");
+  if (roomId) {
+    const room = await db.room.findOne({ roomId }).exec();
+    return res.json(room.users);
+  } else {
+    return res.json([]);
+  }
+});
 
+router.get("/", async (req, res) => {
   const { name, icon, roomId } = req.query;
 
   if (roomId) {
-
     const room = await db.room.findOne({ roomId }).exec();
     const user = new db.user({
       userId: `${name}-${(Math.random() * 100000).toFixed(0)}`,
@@ -14,16 +23,14 @@ router.get("/", async (req, res) => {
       icon,
       status: "empty",
       main: false,
-    })
+    });
 
     room.users.push(user);
     room.save((error) => {
       if (error) res.sendStatus(500);
-      res.json(room)
+      res.json(room);
     });
-
   } else {
-
     const room = new db.room({
       roomId: `room-${name}-${(Math.random() * 100000).toFixed(0)}`,
       typeGame: "Simple",
@@ -34,17 +41,15 @@ router.get("/", async (req, res) => {
           icon,
           status: "active",
           main: true,
-        }
-      ]
+        },
+      ],
     });
 
     room.save((error) => {
       if (error) res.sendStatus(500);
       res.json(room);
     });
-
   }
 });
-
 
 module.exports = router;

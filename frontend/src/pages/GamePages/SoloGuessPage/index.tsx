9a13e6f3e4cdb224d85d96598965cc45b78dc2e9
ldemyanov <leonidemyanov@gmail.com>
2023-02-26 20:56:16 +0800

    fix: change mock data to real data

--- frontend/src/pages/GamePages/SoloGuessPage/index.tsx
+++ frontend/src/pages/GamePages/SoloGuessPage/index.tsx
@@ -8,37 +8,6 @@ import { Button } from '@components/UI/Button';
 import { useNavigate } from 'react-router-dom';
 import { routes } from '@src/routes';
 
-const mockWords = [
-  {
-    word: "Choco Pie",
-    img: "http://localhost:5000/img/image1676636400971.png",
-    response: "Choco Pie",
-  },
-  {
-    word: "Pizza",
-    img: "http://localhost:5000/img/image1676636816533.png",
-    response: "123"
-  },
-  {
-    word:"Car",
-    img:"http://localhost:5000/img/image1676637169461.png",
-    response:"123",
-  },
-  {
-    word:"House",
-    img:"http://localhost:5000/img/image1676637291819.png",
-    response:"123",
-  },
-  {
-    word:"Ant",
-    img:"http://localhost:5000/img/image1676637785308.png",
-    response:"123",
-  },
-]
-
-const mockIcon = "http://localhost:3000/static/media/killer_whale.7db01c230b09e89c02fe.png"
-const mockName = "Leo"
-
 export const SoloResultsPage: FC = () => {
   const { icon, name } = useAppSelector((state) => state.auth);
   const { words } = useAppSelector((state) => state.game.game);
@@ -49,7 +18,7 @@ export const SoloResultsPage: FC = () => {
     <ContentBorder>
       <div className={styles.container}>
         <div className={styles.content}>
-            {mockWords.map((w, i) =>
+            {words.map((w, i) =>
               <Fragment>
                 <div className={`${styles.req} ${styles.appearNow}`}>
                   <img className={styles.reqImg} src={w.img} alt="123" />
@@ -57,7 +26,7 @@ export const SoloResultsPage: FC = () => {
                 </div>
                 <div className={`${styles.messageBlock} ${styles.appearNow}`}>
                   <div className={styles.message}>
-                    <h4 className={styles.name}>{mockName}</h4>
+                    <h4 className={styles.name}>{name}</h4>
                     <div className={styles.text}>
                       {w.response}
                       {checkStrsEqual(w.response as string, w.word) &&
@@ -65,7 +34,7 @@ export const SoloResultsPage: FC = () => {
                     </div>
                   </div>
                   <div className={styles.circle}>
-                    <img className={styles.icon} src={mockIcon} alt={`icon by ${mockName}`}></img>
+                    <img className={styles.icon} src={icon} alt={`icon by ${name}`}></img>
                   </div>
                 </div>
               </Fragment>
